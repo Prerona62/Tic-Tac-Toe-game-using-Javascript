86334a47b5a0b4d23985c41f36018953e03b35fe
Prerona Pattrea <155388170+Prerona62@users.noreply.github.com>
2025-09-16 13:38:53 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,117 @@
+let boxes= document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+let count= 0; //to check for draw condition
+
+//2d arrays-
+// let arr2= [
+//     ["apple", "litchi"],
+//     ["potato", "mushroom"],
+//     ["pants", "shirts"]
+// ];
+
+const winPatterns= [
+	[0, 1, 2],
+	[0, 3, 6],
+	[0, 4, 8],
+	[1, 4, 7],
+	[2, 5, 8],
+	[2, 4, 6],
+	[3, 4, 5],
+	[6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", ( ) => {
+	// console.log("box was clicked");
+	if(turnO) { 
+		//playerO
+		box.innerText= "O";
+		box.style.color = "#084C61";
+		turnO = false;
+	} else{  
+		//playerX
+		box.innerText= "X";
+		box.style.color = "#DB3A34";
+		turnO = true;
+	}
+	box.disabled = true;
+	count++;
+	let isWinner = checkWinner();
+
+	if (count === 9 && !isWinner) {
+   gameDraw();
+	}
+   });
+});
+
+const gameDraw= ( ) => {
+	msg.innerText= "Game was a draw.";
+	msgContainer.classList.remove("hide");
+	disableBoxes( );
+};
+
+const resetGame = ( ) => {
+	turnO= true;
+	count= 0;
+	enableBoxes( );
+	msgContainer.classList.add("hide");
+};
+
+const disableBoxes = ( ) => {
+	for(let box of boxes) {
+	box.disabled = true;
+	}
+};
+
+const enableBoxes = ( ) => {
+	for(let box of boxes) {
+	box.disabled = false;
+	box.innerText= "";
+	box.classList.remove("winning-box"); // remove highlight
+	}
+};
+
+const showWinner = (winner, pattern) => {
+	msg.innerText= `Congratulations! Winner is ${winner}`;
+	msgContainer.classList.remove("hide");
+
+	// highlight winning boxes
+  		pattern.forEach((index) => {
+    	boxes[index].classList.add("winning-box");
+ 	 });
+
+	disableBoxes();
+};
+
+const checkWinner = ( ) => {
+	for(let pattern of winPatterns) {
+// 	console.log(pattern[0], pattern[1], pattern[2]);
+// 	// console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
+// 	console.log(
+// 		boxes[pattern[0]].innerText, 
+// 		boxes[pattern[1]].innerText,
+//  		boxes[pattern[2]].innerText
+// );
+	let pos1val = boxes[pattern[0]].innerText;
+	let pos2val = boxes[pattern[1]].innerText;
+	let pos3val = boxes[pattern[2]].innerText;
+
+	if(pos1val != "" && pos2val != "" && pos3val != "") {
+		if(pos1val === pos2val && pos2val === pos3val) {
+				// console.log("winner");
+				// console.log("winner", pos1val);
+				showWinner(pos1val, pattern);
+				return true;
+	  		}
+		}
+	}
+};
+
+newGameBtn.addEventListener("click", resetGame );
+resetBtn.addEventListener("click", resetGame );
